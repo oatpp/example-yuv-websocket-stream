2d0940827ccdf5faff729ce0af760ad96c73a673
lganzzzo <lganzzzo@gmail.com>
2020-05-26 03:00:07 +0300

    Add dummy tests.

---
+++ test/tests.cpp
@@ -0,0 +1,41 @@
+
+#include "oatpp-test/UnitTest.hpp"
+#include <iostream>
+
+namespace {
+
+  class Test : public oatpp::test::UnitTest {
+  public:
+    Test() : oatpp::test::UnitTest("MyTag")
+    {}
+
+    void onRun() override {
+      // TODO write correct  tests
+      OATPP_LOGW("TESTS", "TODO - WRITE TESTS!");
+    }
+  };
+
+  void runTests() {
+    OATPP_RUN_TEST(Test);
+  }
+
+}
+
+int main() {
+
+  oatpp::base::Environment::init();
+
+  runTests();
+
+  /* Print how much objects were created during app running, and what have left-probably leaked */
+  /* Disable object counting for release builds using '-D OATPP_DISABLE_ENV_OBJECT_COUNTERS' flag for better performance */
+  std::cout << "\nEnvironment:\n";
+  std::cout << "objectsCount = " << oatpp::base::Environment::getObjectsCount() << "\n";
+  std::cout << "objectsCreated = " << oatpp::base::Environment::getObjectsCreated() << "\n\n";
+
+  OATPP_ASSERT(oatpp::base::Environment::getObjectsCount() == 0);
+
+  oatpp::base::Environment::destroy();
+
+  return 0;
+}
